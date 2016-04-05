8bdb3f5eb7c501e7c09631daea4e5b93e7670178
Keisuke Ishizawa <k_ishizawa@sfidante.co.jp>
2016-04-05 20:41:51 +0900

    Add chord sounds

--- app/values/chord.rb
+++ app/values/chord.rb
@@ -1,6 +1,15 @@
 class Chord
-  attr_reader :name
+  attr_reader :name, :sounds
+
+  class << self
+    def chord_sounds
+      path = File.join(Rails.root, "lib/assets/chord_sounds.yml")
+      YAML.load(File.read(path))
+    end
+  end
+
   def initialize(name)
     @name = name
+    @sounds = self.class.chord_sounds[@name]
   end
 end
