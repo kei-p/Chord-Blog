f0ade914acd2cc4670190c184cfe82b8c57d54e0
Keisuke Ishizawa <muddyapesjm66@gmail.com>
2016-04-21 21:13:23 +0900

    Suport parse validation

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,6 +1,16 @@
 class Entry < ActiveRecord::Base
   belongs_to :author
 
+  validate :validate_parse_body
+
+  def validate_parse_body
+    begin
+      ChordParser.new(body).parse
+    rescue => e
+      errors.add(:body, e.message)
+    end
+  end
+
   def author?(author)
     self.author == author
   end
